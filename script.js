8c459fc5a653962a3d38180fc718dd50e083fae8
fahim- hossen <fahim.fahim3408@gmail.com>
2024-09-08 14:05:34 +0600

    notun

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+let cart = document.querySelector('.shopping-cart');
 
+document.querySelector('#cart').onclick = () =>{
+  cart.classList.toggle('active');
+  login.classList.remove('active');
+}
 
+let login = document.querySelector('.login-form');
+
+document.querySelector('#login').onclick = () =>{
+  login.classList.toggle('active');
+  cart.classList.remove('active');
+}
 
 let navbar = document.querySelector('.navbar');
 
@@ -9,7 +20,11 @@ document.querySelector('#menu').onclick = () =>{
   login.classList.remove('active');
 }
 
-
+window.onscroll = () =>{
+  cart.classList.remove('active');
+  login.classList.remove('active');
+  navbar.classList.remove('active');
+}
 
 var swiper = new Swiper(".home-slider", {
   autoplay:{
@@ -26,7 +41,17 @@ var swiper = new Swiper(".home-slider", {
 });
 
 
-
+var swiper = new Swiper(".menu-slider", {
+  grabCursor:true,
+  loop: true,
+  autoHeight:true,
+  centeredSlides:true,
+  spaceBetwwen:20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+  },
+});
 
 let previewContainer = document.querySelector('.menu-preview-container');
 let previewBox = previewContainer.querySelectorAll('.menu-preview');
@@ -50,3 +75,6 @@ previewContainer.querySelector('#close').onclick = () =>{
     close.classList.remove('active');
   });
 };
+
+
+
